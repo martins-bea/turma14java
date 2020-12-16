fdf15d921ce8c1fc9d879956180269a2d2f066bf
unknown <beatriz.martins.15@hotmail.com>
2020-12-15 23:52:40 -0300

    Objetos POO

---
+++ aulasJava/src/POO/Exercicio2AviaoObjetoJava.java
@@ -0,0 +1,35 @@
+package POO;
+
+public class Exercicio2AviaoObjetoJava {
+
+
+		public String Aviao;
+		public double velocidadeMinima;
+		public double velocidadeMaxima;
+		public boolean EstadoAviao;
+		
+		public Exercicio2AviaoObjetoJava()
+		{
+			
+		}
+		public Exercicio2AviaoObjetoJava(double velocidadeMinima, double velocidadeMaxima)
+		{
+			this.velocidadeMinima = velocidadeMinima;
+			this.velocidadeMaxima = velocidadeMaxima;
+		}
+		
+		public void EstadoAviao(char aviao)
+		{
+			if(EstadoAviao == true)
+			{
+				System.out.println("Avião Ligado");
+			}else {
+				System.out.println("Avião Desligado");
+			}
+			
+		}
+		
+}
+
+
+
